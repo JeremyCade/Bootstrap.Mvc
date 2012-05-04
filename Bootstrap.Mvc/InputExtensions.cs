fb18c97857fa2598a1800c9d3eba8fdc281bfb0c
Jeremy Cade <jeremy.cade@gmail.com>
2012-05-04 14:35:35 +1000

    Updated LabelCheckBoxFor extensions

--- Bootstrap.Mvc/InputExtensions.cs
+++ Bootstrap.Mvc/InputExtensions.cs
@@ -20,15 +20,30 @@ namespace Bootstrap.Mvc
     {
         public static MvcHtmlString LabelCheckBoxFor<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression)
         {
-            return LabelCheckBoxFor(htmlHelper, expression, null);
+            return LabelCheckBoxFor(htmlHelper, expression, null, null);
         }
 
         public static MvcHtmlString LabelCheckBoxFor<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression, object htmlAttributes)
         {
-            return LabelCheckBoxFor(htmlHelper, expression, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+            return LabelCheckBoxFor(htmlHelper, expression, null, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
         }
 
         public static MvcHtmlString LabelCheckBoxFor<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression, IDictionary<string, object> htmlAttributes)
+        {
+            return LabelCheckBoxFor(htmlHelper, expression, null, htmlAttributes);
+        }
+
+        public static MvcHtmlString LabelCheckBoxFor<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression, string labelText) 
+        {
+            return LabelCheckBoxFor(htmlHelper, expression, labelText, null);
+        }
+
+        public static MvcHtmlString LabelCheckBoxFor<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression, string labelText, object htmlAttributes) 
+        {
+            return LabelCheckBoxFor(htmlHelper, expression, labelText, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+        }
+
+        public static MvcHtmlString LabelCheckBoxFor<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression, string labelText = null, IDictionary<string, object> htmlAttributes = null)
         {
             if (expression == null)
             {
@@ -48,18 +63,22 @@ namespace Bootstrap.Mvc
                 }
             }
 
-            string labelText = metadata.DisplayName ?? metadata.PropertyName ?? htmlFieldName.Split('.').Last();
-            if (string.IsNullOrEmpty(labelText))
+            string resolvedLabelText = labelText ?? metadata.DisplayName ?? metadata.PropertyName ?? htmlFieldName.Split('.').Last();
+            if (String.IsNullOrEmpty(resolvedLabelText))
             {
                 return MvcHtmlString.Empty;
-            }            
-            
+            }
+
+
             TagBuilder label = new TagBuilder("label");
-            label.MergeAttributes(htmlAttributes);
-            label.MergeAttribute("class","checkbox"); // Add Twitter Bootstrap checkbox style to the label.
-            label.Attributes.Add("for", htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldId(htmlFieldName));            
-            label.InnerHtml = String.Format("{0}{1}",htmlHelper.CheckBox(htmlFieldName, isChecked).ToHtmlString(), labelText);            
-            return label.ToMvcHtmlString(TagRenderMode.Normal);
+            label.MergeAttributes(htmlAttributes, replaceExisting: true);
+            label.MergeAttribute("class", "checkbox"); // Add Twitter Bootstrap checkbox style to the label.
+            label.Attributes.Add("for", htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldId(htmlFieldName));
+            label.InnerHtml = String.Format("{0}{1}", htmlHelper.CheckBox(htmlFieldName, isChecked).ToHtmlString(), resolvedLabelText);
+
+            return label.ToMvcHtmlString(TagRenderMode.Normal);        
         }
+
+        
     }
 }
